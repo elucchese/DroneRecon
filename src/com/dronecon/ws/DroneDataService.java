62eb8fab7ac119b18f726fc74a9a9b15ef382256
emilylucchese <schrader_emily@me.com>
2022-05-17 12:50:03 -0700

    updated DroneDataService.java

--- src/com/dronecon/ws/DroneDataService.java
+++ src/com/dronecon/ws/DroneDataService.java
@@ -2,14 +2,14 @@ package com.dronecon.ws;
         import javax.servlet.*;
         import javax.servlet.http.*;
         import java.io.*;
+        import java.net.URL;
         import java.util.*;
         import java.security.SecureRandom;
 
     /**
-     *
      * @author Emily
      */
-    public class DroneDataService extends HttpServlet{
+    public class DroneDataService extends HttpServlet{ //servlet in apache
 
 
         public void doGet(HttpServletRequest request,
@@ -23,26 +23,32 @@ package com.dronecon.ws;
 
             // ##############################
             // 1. Get params passed in.
-
-            // Get the following parameters from the request object and put them into strings:
-            // area_id
-            // tilex
-            // tiley
-            // totalcols
-            // totalrows
             // ##############################
-            String area_id = request.getParameter("area_id");
-            String tilex = request.getParameter("tilex");
-            String tiley = request.getParameter("tiley");
-            String totalcols = request.getParameter("totalcols");
-            String totalrows = request.getParameter("totalrows");
+            String sArea_id = request.getParameter("area_id");
+            int iTilex = Integer.parseInt(request.getParameter("tilex"));
+            int iTiley = Integer.parseInt(request.getParameter("tiley"));
+            int totalcols = Integer.parseInt(request.getParameter("totalcols"));
+            int totalrows = Integer.parseInt(request.getParameter("totalrows"));
 
+            //###############################
+            String sRed = request.getParameter("r");
+            String sGreen = request.getParameter("g");
 
+            //################################
+            //Send data for this tile to the cloud (PortalDBService.java)
+            //call PortalDBService api and send this tile's data
+            try {
+                URL url = new URL("http://127.0.0.1:8080/dronereconportal/PortalDBService?area_id="
+                        + sArea_id +"&tilex=" + iTilex + "&tiley="+ iTiley + "&r="+ sRed + "+&g=" + sGreen);
+                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("An error happened when calling PortalDBService api: " + ex.toString());
+            }
 
             // ##############################
             // 2. Default value of beginning direction.
-
-            // Set a string called sDirection to "right".
             // ##############################
             String sDirection = "right";
 
@@ -50,24 +56,44 @@ package com.dronecon.ws;
 
             // ##############################
             // 3. Calculate next drone move.
-
-            // Determine next tile to move to.
-            // Base this on current x and y.
-            // Change sDirection if necessary.
-            // Drone must serpentine from top left of grid back and forth down.
-            // If rows are done, change sDirection to "stop".
             // ##############################
 
+            //see if on even row
+            if(iTiley % 2 == 0){
+                //check if on last tile of row
+                if(iTilex == totalcols - 1){
+                    iTiley++;
+                    sDirection = "left";
+                }// else x needs to be increased
+                else {
+                    iTilex++;
+                    sDirection = "right";
+                }
 
+            }//else it is an odd row
+            else {
+                //check if on far left tile
+                if (iTilex == 0) {
+                    iTiley++;
+                    sDirection = "right";
+                }
+                //else x need to be drecreased
+                else {
+                    iTilex--;
+                    sDirection = "left";
+                }
+
+            } //check if off grid because then time to stop
+                if(iTiley == totalrows){
+                    sDirection = "stop";
+                }
 
             // ##############################
             // 4. Format & Return JSON string to caller.
-
-        /* Return via out.println() a JSON string like this:
-        {"area_id":"[area id from above]", "nextTileX":"[next tile x]", "nextTileY":"[next tile y]", "direction":"[direction string from above]"}
-        */
             // ##############################
 
+            String sReturnJson = "{\"area_id\":\"" + sArea_id + "\", \"nextTileX\":\" "+ iTilex +" \", \"nextTileY\":\" "+ iTiley +" \", \"direction\":\" "+sDirection+" \"}";
+            out.println(sReturnJson);
         }
     }
 
